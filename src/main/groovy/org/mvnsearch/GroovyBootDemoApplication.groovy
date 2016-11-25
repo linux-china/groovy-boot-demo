e6521a127096c30f7fa30bf3899526f1310b039e
linux_china <linux_china@hotmail.com>
2016-11-25 16:37:04 +0800

    groovy boot application

---
+++ src/main/groovy/org/mvnsearch/GroovyBootDemoApplication.groovy
@@ -0,0 +1,12 @@
+package org.mvnsearch
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class GroovyBootDemoApplication {
+
+  static void main(String[] args) {
+    SpringApplication.run(GroovyBootDemoApplication.class, args);
+  }
+}
