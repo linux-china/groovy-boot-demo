f2b8498c591cc2664a1b2b403e9213c4db035e74
linux_china <linux_china@hotmail.com>
2016-11-25 16:37:34 +0800

    application tests

---
+++ src/test/groovy/org/mvnsearch/GroovyBootDemoApplicationTests.groovy
@@ -0,0 +1,22 @@
+package org.mvnsearch
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+/**
+ * application tests
+ *
+ * @author linux_china
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class GroovyBootDemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+    println("Hello Groovy Boot application")
+  }
+
+}
