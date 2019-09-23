1bf417e0f5e2806fe2f10756565e3403d6f1508d
linux_china <libing.chen@gmail.com>
2019-09-23 09:39:17 -0700

    Migrate to JUnit 5

--- src/test/groovy/org/mvnsearch/GroovyBootDemoApplicationTests.groovy
+++ src/test/groovy/org/mvnsearch/GroovyBootDemoApplicationTests.groovy
@@ -1,16 +1,13 @@
 package org.mvnsearch
 
-import org.junit.Test
-import org.junit.runner.RunWith
+import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit4.SpringRunner
 
 /**
  * application tests
  *
  * @author linux_china
  */
-@RunWith(SpringRunner.class)
 @SpringBootTest
 class GroovyBootDemoApplicationTests {
 
